f0ad086d1f03a01bab3cbd8e07e197460fca1e21
ViniciusDSDSouza <viniciusdossantos547@gmail.com>
2025-04-10 18:58:32 -0300

    refactor: move listen to server.js to improve testability

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,6 @@
-import express from "express";
-import cors from "cors";
-import { FRONTEND_REDIRECT_INDEX } from "./config/env.js";
+import app from "./app.js";
 
-import { getRepos } from "./handlers/getRepos.js";
-import { postRepos } from "./handlers/postRepos.js";
-import { deleteRepos } from "./handlers/deleteRepos.js";
-import { getAuthGithub } from "./handlers/getAuthGithub.js";
-import { getAuthCallback } from "./handlers/getAuthCallback.js";
-
-const app = express();
-const port = 3000;
-
-app.use(express.json());
-
-// CORS para comunicação com FRONTEND
-app.use(
-  cors({
-    origin: FRONTEND_REDIRECT_INDEX, // Permitir apenas o frontend
-    methods: ["GET", "POST", "OPTIONS", "DELETE"], // Métodos permitidos
-    allowedHeaders: ["Content-Type", "Authorization"], // Cabeçalhos permitidos
-  })
-);
-
-app.get("/auth/github", getAuthGithub);
-
-app.get("/auth/callback", getAuthCallback);
-
-app.get("/repos", getRepos);
-
-app.post("/repos", postRepos);
-
-app.delete("/repos/:owner/:repo", deleteRepos);
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server running at port: ${port}`);
